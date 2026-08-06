c60878f91484779ff3a9c4a450bdbbc4de41ad76
loglot <prattlog3@gmail.com>
2026-08-06 11:58:42 -0500

    fixed make script

--- make.sh
+++ make.sh
@@ -2,10 +2,7 @@ rm -r out
 npm run make
 mkdir out/HARDHAT-Linux-x64
 mv out/hardhat-linux-x64 out/HARDHAT-Linux-x64/
-mv out/HARDHAT-Linux-x64/hardhat-linux-x64/resources/app/install2.x.sh out/HARDHAT-Linux-x64/hardhat-linux-x64/
-mv out/HARDHAT-Linux-x64/hardhat-linux-x64/resources/app/find.sh out/HARDHAT-Linux-x64/hardhat-linux-x64/
-mv out/HARDHAT-Linux-x64/hardhat-linux-x64/resources/app/installMod.sh out/HARDHAT-Linux-x64/hardhat-linux-x64/
-mv out/HARDHAT-Linux-x64/hardhat-linux-x64/resources/app/manageMod.sh out/HARDHAT-Linux-x64/hardhat-linux-x64/
+mv out/HARDHAT-Linux-x64/hardhat-linux-x64/resources/app/SH out/HARDHAT-Linux-x64/hardhat-linux-x64/
 echo cd ./hardhat-linux-x64 > out/HARDHAT-Linux-x64/HARDHAT.sh
 echo ./hardhat >> out/HARDHAT-Linux-x64/HARDHAT.sh
 chmod +x out/HARDHAT-Linux-x64/HARDHAT.sh
